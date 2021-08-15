d7193a811963fbd2da290e9eb04bffc7697b4b52
komcrad <konradt95@gmail.com>
2021-08-15 17:38:47 -0500

    Added a function for making a memoized function with a persistent cache. Naive approach. Not thread safe.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject komcrad-utils "0.12.0"
+(defproject komcrad-utils "0.13.0-SNAPSHOT"
   :description "komcrad's utilities"
   :url "https://github.com/komcrad/komcrad-utils"
   :license {:name "GNU Lesser General Public License"
@@ -8,4 +8,5 @@
                  [nrepl "0.7.0"]
                  [com.taoensso/nippy "3.1.1"]
                  [org.clojure/data.csv "1.0.0"]
-                 [org.exupero/clipboard "0.1.0"]])
+                 [org.exupero/clipboard "0.1.0"]
+                 [org.clojure/core.memoize "1.0.250"]])
