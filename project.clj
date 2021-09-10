68d808fed66400d5b895dc99ac94820cb44f7f00
komcrad <konradt95@gmail.com>
2021-09-09 19:49:17 -0500

    Chug fixes (#17)
    * Fix chug
    
    * Formatting
    
    * Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject komcrad-utils "0.13.0-SNAPSHOT"
+(defproject komcrad-utils "0.12.1"
   :description "komcrad's utilities"
   :url "https://github.com/komcrad/komcrad-utils"
   :license {:name "GNU Lesser General Public License"
@@ -9,4 +9,5 @@
                  [com.taoensso/nippy "3.1.1"]
                  [org.clojure/data.csv "1.0.0"]
                  [org.exupero/clipboard "0.1.0"]
-                 [org.clojure/core.memoize "1.0.250"]])
+                 [org.clojure/core.memoize "1.0.250"]]
+  :profiles {:dev {:dependencies [[circleci/bond "0.6.0"]]}})
